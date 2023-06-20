629c9bb743afcc1ca162c9831dea5737cf0616d0
RaulRPrado <raul.ribeiro.prado@gmail.com>
2023-06-20 19:56:50 -0300

    extract using movie list and exporting files

--- extract.py
+++ extract.py
@@ -1,14 +1,9 @@
 import logging
 import requests
+from os import path
 from bs4 import BeautifulSoup
 
-logging.basicConfig(level=logging.DEBUG)
-
-movies = [
-    'Everything Everywhere All at Once 2022',
-    'Top Gun: Maverick 2022',
-    'The Batman 2022'
-]
+logging.basicConfig(level=logging.INFO)
 
 def GoogleRogerEbertSearchUrl(text):
     words = (text + ' roger ebert movie review').split(' ')
@@ -17,7 +12,7 @@ def GoogleRogerEbertSearchUrl(text):
 
 def RogerEbertReviewUrl(movie):
     searchUrl = GoogleRogerEbertSearchUrl(movie)
-    logging.debug('Google Search - ' + searchUrl)
+    logging.info('Google Search - ' + searchUrl)
 
     content = requests.get(searchUrl).text
     url_start_index = content.find('https://www.rogerebert.com/reviews/')
@@ -26,7 +21,7 @@ def RogerEbertReviewUrl(movie):
     url_end_index = start_content.find('&')
     url = start_content[:url_end_index]
 
-    logging.debug('Roger Ebert Url - ' + searchUrl)
+    logging.info('Roger Ebert Url - ' + url)
 
     return url
 
@@ -43,9 +38,46 @@ def ExtractRogerEbertReview(url):
 
 if __name__=="__main__":
 
-    for movie in movies:
-        url = RogerEbertReviewUrl(movie)
-        print(url)
+    maxMovies = 100
 
-        review = ExtractRogerEbertReview(url)
-        print(review)
+    with open('movies.dat', 'r') as file:
+
+        count = 0
+        for row in file:
+            movie = row.rstrip('\n')
+            if count >= maxMovies:
+                break
+
+            logging.info(movie)
+
+            # RogerEbert
+            fileName = 'reviews/RogerEbert/' + movie + '.txt'
+            fileNameError = 'reviews/RogerEbert/' + movie + '_ERROR.txt'
+            if path.exists(fileName):
+                logging.info('Review already exists')
+                continue
+
+            if path.exists(fileNameError):
+                logging.info('Error file already exists')
+                continue
+
+            url = RogerEbertReviewUrl(movie)
+            if len(url) < 10:
+                msg = 'Url is invalid - error {}'.format(movie)
+                logging.error(msg)
+                with open(fileNameError, 'w') as f:
+                    f.write(msg)
+                continue
+
+            review = ExtractRogerEbertReview(url)
+
+            if len(review) < 200:
+                msg = 'Review is too short - error {}'.format(movie)
+                logging.error(msg)
+                with open(fileNameError, 'w') as f:
+                    f.write(msg)
+                continue
+
+            with open(fileName, 'w') as reviewFile:
+                reviewFile.write(review)
+                count += 1
