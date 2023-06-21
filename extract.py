121662b9c0a34576a988a8690e5b5bd92f81a5fd
RaulRPrado <raul.ribeiro.prado@gmail.com>
2023-06-20 21:15:07 -0300

    extract with lib + hollywood reporter

--- extract.py
+++ extract.py
@@ -1,58 +1,32 @@
 import logging
-import requests
 from os import path
-from bs4 import BeautifulSoup
+
+from extract_lib import ExtractReview, ReviewUrl
 
 logging.basicConfig(level=logging.INFO)
 
-def GoogleRogerEbertSearchUrl(text):
-    words = (text + ' roger ebert movie review').split(' ')
-    search_words = "+".join(words)
-    return 'https://google.com/search?q=' + search_words
-
-def RogerEbertReviewUrl(movie):
-    searchUrl = GoogleRogerEbertSearchUrl(movie)
-    logging.info('Google Search - ' + searchUrl)
-
-    content = requests.get(searchUrl).text
-    url_start_index = content.find('https://www.rogerebert.com/reviews/')
-    start_content = content[url_start_index:]
-
-    url_end_index = start_content.find('&')
-    url = start_content[:url_end_index]
-
-    logging.info('Roger Ebert Url - ' + url)
-
-    return url
-
-def ExtractRogerEbertReview(url):
-    page = requests.get(url).content
-    soup = BeautifulSoup(page, "html.parser")
-    blocks = soup.find_all('section', {'class': 'page-content--block_editor-content js--reframe'})
-
-    review = ''
-    for block in blocks:
-        review += ' ' + block.get_text()
-
-    return review
-
 if __name__=="__main__":
 
-    maxMovies = 100
+    maxMovies = 10
+    source = 'HollywoodReporter'
+
+    if source not in ['RogerEbert', 'HollywoodReporter']:
+        logging.error('Source is invalid')
+        exit()
 
     with open('movies.dat', 'r') as file:
 
         count = 0
         for row in file:
-            movie = row.rstrip('\n')
+            movie = row.rstrip('\n').replace('/', '')
             if count >= maxMovies:
                 break
 
             logging.info(movie)
 
-            # RogerEbert
-            fileName = 'reviews/RogerEbert/' + movie + '.txt'
-            fileNameError = 'reviews/RogerEbert/' + movie + '_ERROR.txt'
+            dirName = 'reviews/' + source + '/'
+            fileName = dirName + movie + '.txt'
+            fileNameError = dirName + movie + '_ERROR.txt'
             if path.exists(fileName):
                 logging.info('Review already exists')
                 continue
@@ -61,7 +35,7 @@ if __name__=="__main__":
                 logging.info('Error file already exists')
                 continue
 
-            url = RogerEbertReviewUrl(movie)
+            url = ReviewUrl(source, movie)
             if len(url) < 10:
                 msg = 'Url is invalid - error {}'.format(movie)
                 logging.error(msg)
@@ -69,7 +43,11 @@ if __name__=="__main__":
                     f.write(msg)
                 continue
 
-            review = ExtractRogerEbertReview(url)
+            review = ExtractReview(source, url)
+
+            # print(review)
+            # count += 1
+            # continue
 
             if len(review) < 200:
                 msg = 'Review is too short - error {}'.format(movie)
