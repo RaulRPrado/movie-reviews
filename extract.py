7ce8755c1e43d3f9e0df22c43281ef77d0e49675
RaulRPrado <raul.ribeiro.prado@gmail.com>
2023-06-21 21:24:57 -0300

    new sources into extract

--- extract.py
+++ extract.py
@@ -12,7 +12,7 @@ parser.add_argument(
     "--src",
     default=None,
     type=str,
-    choices=['RogerEbert', 'HollywoodReporter']
+    choices=['RogerEbert', 'HollywoodReporter', 'EW', 'EmpireOnline', 'RollingStone']
 )
 parser.add_argument(
     "--max",
@@ -24,6 +24,8 @@ parser.add_argument(
 if __name__=="__main__":
 
     args = parser.parse_args()
+
+    url_file = open('url_log.csv', 'a')
 
     with open('movies.dat', 'r') as file:
 
@@ -52,21 +54,27 @@ if __name__=="__main__":
                 logging.error(msg)
                 with open(fileNameError, 'w') as f:
                     f.write(msg)
+                count += 1
                 continue
+
+            url_file.write(movie + ',' + url + '\n')
 
             review = ExtractReview(args.src, url)
 
-            # print(review)
-            # count += 1
-            # continue
+            #print(review)
+            #count += 1
+            #continue
 
             if len(review) < 200:
                 msg = 'Review is too short - error {}'.format(movie)
                 logging.error(msg)
                 with open(fileNameError, 'w') as f:
                     f.write(msg)
+                count += 1
                 continue
 
             with open(fileName, 'w') as reviewFile:
                 reviewFile.write(review)
                 count += 1
+
+    url_file.close()
