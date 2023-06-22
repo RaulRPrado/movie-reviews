0ffded1b61d8775cf2b2962002af61fcb190678c
RaulRPrado <raul.ribeiro.prado@gmail.com>
2023-06-22 16:04:35 -0300

    moving functions to utils

--- utils.py
+++ utils.py
@@ -1,2 +1,16 @@
 def getMovieYear(movie):
     return movie[-4:]
+
+
+def getReviewFileName(source, movie):
+    dirName = 'reviews/' + source + '/'
+    return dirName + movie + '.txt'
+
+
+def getReviewFileNameError(source, movie):
+    dirName = 'reviews/' + source + '/'
+    return dirName + movie + '_ERROR.txt'
+
+def getMovieName(row):
+    return row.rstrip('\n').replace('/', '')
+
